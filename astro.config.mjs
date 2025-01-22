1dec5ab782353928f5540fc7abc7703d7a7a6ce5
David Burnham <dburnham@golfscoring.net>
2025-01-22 06:19:49 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://david9930.github.io',
   base: '/living-with-the-ghost-of-sam',
+  integrations: [react()],
 });
