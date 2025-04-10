6ffdff08046e6128e104a9e63cb1a42192de2d54
David Burnham <dburnham@golfscoring.net>
2025-04-10 10:40:21 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-// In astro.config.mjs
 export default defineConfig({
   site: 'https://lwtgos.com',
-  // Remove the "base" property if it exists
-  // Or set it to "/"
+  integrations: [react()],
 });
